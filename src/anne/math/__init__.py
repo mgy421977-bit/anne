2abe05823ac1ca7096dfb4f0d66e1c0cc9d45e1a
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-05 19:48:34 +0300

    feat: add ANNE math core package

---
+++ src/anne/math/__init__.py
@@ -0,0 +1,5 @@
+"""ANNE mathematical computation layer."""
+from anne.math.engine import CalculationResult, MathEngine
+from anne.math.derivation import DerivationResult, SymbolicDerivationEngine
+
+__all__ = ["CalculationResult", "MathEngine", "DerivationResult", "SymbolicDerivationEngine"]
