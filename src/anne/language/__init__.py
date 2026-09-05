b7516dfed2f06f79ee198c167c1388eb708fd06e
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-05 19:07:29 +0300

    feat: add symbolic Turkish language engine

---
+++ src/anne/language/__init__.py
@@ -0,0 +1,4 @@
+"""ANNE language layer: symbolic vocabulary, morphology and grammar."""
+from anne.language.engine import LanguageEngine, LanguageAnalysis
+
+__all__ = ["LanguageEngine", "LanguageAnalysis"]
