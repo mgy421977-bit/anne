1105d9b8bf2774640c8f843e1b74b6c26682aa25
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-06 01:48:19 +0300

    feat: export MITOS research swarm primitives

--- src/anne/mythos/__init__.py
+++ src/anne/mythos/__init__.py
@@ -1,11 +1,28 @@
-"""MITOS exploration layer for ANNE."""
+"""MITOS exploration and bounded research-swarm layer for ANNE."""
 
+from .agent_swarm import (
+    AgentRole,
+    EvidenceItem,
+    EvidencePackage,
+    MitosAgentSwarm,
+    ResearchAgent,
+    ResearchMission,
+    ResourceGovernor,
+)
 from .discovery import DiscoveryDrive, Evaluation
 from .engine import ExplorationMode, HypothesisCandidate, MitosEngine
 from .experience import ExperienceRecord, ExperienceStatus
 from .loop import DiscoveryBatch, MitosAnneLoop
+from .synthesis import MitosSynthesis, SynthesisFinding
 
 __all__ = [
+    "AgentRole",
+    "EvidenceItem",
+    "EvidencePackage",
+    "MitosAgentSwarm",
+    "ResearchAgent",
+    "ResearchMission",
+    "ResourceGovernor",
     "DiscoveryDrive",
     "Evaluation",
     "ExplorationMode",
@@ -15,4 +32,6 @@ __all__ = [
     "ExperienceStatus",
     "DiscoveryBatch",
     "MitosAnneLoop",
+    "MitosSynthesis",
+    "SynthesisFinding",
 ]
