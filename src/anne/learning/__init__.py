e0b2c016a470b5ee1ccf6e55493054af7f07f8d2
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-06 16:57:39 +0300

    feat: export capability memory

--- src/anne/learning/__init__.py
+++ src/anne/learning/__init__.py
@@ -1,7 +1,15 @@
 """Evidence-gated learning primitives for ANNE."""
 
+from .capability_memory import CapabilityMemory
 from .evidence import EvidenceItem, LearningCandidate, LearningResult
-from .web_research import WebResearcher
 from .percentage import PercentageLearner
+from .web_research import WebResearcher
 
-__all__ = ["EvidenceItem", "LearningCandidate", "LearningResult", "WebResearcher", "PercentageLearner"]
+__all__ = [
+    "CapabilityMemory",
+    "EvidenceItem",
+    "LearningCandidate",
+    "LearningResult",
+    "PercentageLearner",
+    "WebResearcher",
+]
