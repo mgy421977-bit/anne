8f45e97bde7461f9e3f261ede64d54ef91c2fd18
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-06 17:19:56 +0300

    feat: export knowledge resolution components

--- src/anne/learning/__init__.py
+++ src/anne/learning/__init__.py
@@ -1,9 +1,11 @@
-"""Evidence-gated learning primitives for ANNE."""
+"""Evidence-gated learning and knowledge primitives for ANNE."""
 
 from .capability_memory import CapabilityMemory
 from .capability_registry import CapabilityRegistry
 from .evidence import EvidenceItem, LearningCandidate, LearningResult
 from .greeting import GreetingLearner
+from .knowledge_memory import KnowledgeMemory
+from .knowledge_resolver import KnowledgeResolution, KnowledgeResolver
 from .percentage import PercentageLearner
 from .web_research import WebResearcher
 
@@ -12,6 +14,9 @@ __all__ = [
     "CapabilityRegistry",
     "EvidenceItem",
     "GreetingLearner",
+    "KnowledgeMemory",
+    "KnowledgeResolution",
+    "KnowledgeResolver",
     "LearningCandidate",
     "LearningResult",
     "PercentageLearner",
