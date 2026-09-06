a6c353f3008eee6b075f351df6c493c73094b59f
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-06 17:03:45 +0300

    feat: export general capability registry and greeting learner

--- src/anne/learning/__init__.py
+++ src/anne/learning/__init__.py
@@ -1,13 +1,17 @@
 """Evidence-gated learning primitives for ANNE."""
 
 from .capability_memory import CapabilityMemory
+from .capability_registry import CapabilityRegistry
 from .evidence import EvidenceItem, LearningCandidate, LearningResult
+from .greeting import GreetingLearner
 from .percentage import PercentageLearner
 from .web_research import WebResearcher
 
 __all__ = [
     "CapabilityMemory",
+    "CapabilityRegistry",
     "EvidenceItem",
+    "GreetingLearner",
     "LearningCandidate",
     "LearningResult",
     "PercentageLearner",
